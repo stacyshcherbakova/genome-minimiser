0639790277c1de5f5008d3c256f02add16649a74
stacyshcherbakova <stacyshcherbakova@gmail.com>
2024-06-11 19:01:45 +0100

    new file added (does not work for some reason)

--- dir.py
+++ dir.py
@@ -2,5 +2,6 @@
 
 PANGENOME_MATRIX_CSV = "pangenome_matrix.csv"
 PANGENOME_MATRIX_TXT = "pangenome_matrix.txt"
+PHYLOGROUPS_DATA = "accessionID_phylogroup2.csv"
 BASE_DIR = "/Users/anastasiiashcherbakova/git_projects/masters_project"
 OUT_DIR = "/Users/anastasiiashcherbakova/git_projects/masters_project"
