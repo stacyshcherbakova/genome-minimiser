d2b346716f35f8b91dbbf0623c1705631cf301e9
stacyshcherbakova <stacyshcherbakova@gmail.com>
2024-06-13 19:04:14 +0100

    version 2.0

--- dir.py
+++ dir.py
@@ -1,7 +1,9 @@
 # Paths to data and other 
 
-PANGENOME_MATRIX_CSV = "pangenome_matrix.csv"
-PANGENOME_MATRIX_TXT = "pangenome_matrix.txt"
-PHYLOGROUPS_DATA = "accessionID_phylogroup2.csv"
+PANGENOME_MATRIX_CSV = "/Users/anastasiiashcherbakova/git_projects/masters_project/data/pangenome_matrix.csv"
+PANGENOME_MATRIX_TXT = "/Users/anastasiiashcherbakova/git_projects/masters_project/data/pangenome_matrix.txt"
+PHYLOGROUPS_DATA = "/Users/anastasiiashcherbakova/git_projects/masters_project/data/accessionID_phylogroup2.csv"
+TEN_K_DATASET = "/Users/anastasiiashcherbakova/git_projects/masters_project/data/F4_complete_presence_absence.csv"
+
 BASE_DIR = "/Users/anastasiiashcherbakova/git_projects/masters_project"
 OUT_DIR = "/Users/anastasiiashcherbakova/git_projects/masters_project"
