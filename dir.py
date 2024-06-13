b01a8b4af21da59e9ac5c77fbf46ab84442a778b
stacyshcherbakova <stacyshcherbakova@gmail.com>
2024-06-13 11:04:36 +0100

    added new file name

--- dir.py
+++ dir.py
@@ -3,5 +3,7 @@
 PANGENOME_MATRIX_CSV = "pangenome_matrix.csv"
 PANGENOME_MATRIX_TXT = "pangenome_matrix.txt"
 PHYLOGROUPS_DATA = "accessionID_phylogroup2.csv"
+TEN_K_DATASET = "F4_complete_presence_absence.csv"
+
 BASE_DIR = "/Users/anastasiiashcherbakova/git_projects/masters_project"
 OUT_DIR = "/Users/anastasiiashcherbakova/git_projects/masters_project"
