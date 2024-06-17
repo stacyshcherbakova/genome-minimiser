6caabb3f1b582e1363416bac4f2e9ea9874fe4f0
stacyshcherbakova <stacyshcherbakova@gmail.com>
2024-06-17 17:35:11 +0100

    version 3.0

--- train_full_dataset.py
+++ train_full_dataset.py
@@ -2,6 +2,7 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
+import sklearn
 from VAE_model import *
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
@@ -10,33 +11,58 @@ from extras import *
 plt.style.use('ggplot')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+folder = "3_train/"
+
 print("** START OF THE SCRIPT **\n")
 
 ## Loading and preping the dataset
 print("LOADING THE DATASET...")
 large_data = pd.read_csv("F4_complete_presence_absence.csv", index_col=[0], header=[0])
+large_data.columns = large_data.columns.str.upper()
+phylogroup_data = pd.read_csv("accessionID_phylogroup_BD.csv", index_col=[0], header=[0])
+data_without_lineage = large_data.drop(index=['Lineage'])
+merged_df = pd.merge(data_without_lineage.transpose(), phylogroup_data, how='inner', left_index=True, right_on='ID')
 
-large_data_t = np.array(large_data.transpose())
-large_data_t = large_data_t[:,1:]
-print(f"Dataset shape: {large_data_t.shape}")
+numeric_cols = merged_df.select_dtypes(include='number')
+column_sums = numeric_cols.sum(axis=0)
+
+filtered_columns = column_sums[column_sums / 7512 >= 0.05].index
+filtered_data = merged_df[filtered_columns]
+
+filtered_data = merged_df[filtered_columns].copy()
+filtered_data['Phylogroup'] = merged_df['Phylogroup']
+
+data_array_t = np.array(filtered_data.iloc[:, :-1])
+phylogroups_array = np.array(filtered_data.iloc[:, -1])
+
+print(f"Dataset shape: {data_array_t.shape}")
+print(f"Phylogroups array shape: {phylogroups_array.shape}")
 
 ## Preping the dataset
 print("PREPING THE DATASET...")
 # Convert to PyTorch tensor
-data_tensor = torch.tensor(large_data_t, dtype=torch.float32)
+data_tensor = torch.tensor(data_array_t, dtype=torch.float32)
 
 # Split into train and test sets
-train_data, val_data = train_test_split(data_tensor, test_size=0.2, random_state=12345)
-train_data, test_data = train_test_split(data_tensor, test_size=0.25, random_state=12345)
+# train_data, val_data = train_test_split(data_tensor, test_size=0.2, random_state=12345)
+# train_data, test_data = train_test_split(data_tensor, test_size=0.25, random_state=12345)
+
+# Spliting into train and test sets
+train_data, temp_data, train_labels, temp_labels = train_test_split(data_tensor, phylogroups_array, test_size=0.3, random_state=12345)
+val_data, test_data, val_labels, test_labels = train_test_split(temp_data, temp_labels, test_size=0.3333, random_state=12345)
+
+test_phylogroups = test_labels
 
 # TensorDataset
 train_dataset = TensorDataset(train_data)
 val_dataset = TensorDataset(val_data)
+test_dataset = TensorDataset(test_data)
 
 # DataLoaders for main training
 batch_size = 32
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 ## Preping model inputs
 print("PREPPING MODEL INPUTS...")
@@ -45,9 +71,10 @@ hidden_dim = 512
 latent_dim = 64
 beta_start = 0.1
 beta_end = 1.0
-n_epochs = 10
+n_epochs = 1
 max_norm = 1.0 
-input_dim = large_data_t.shape[1]
+input_dim = data_array_t.shape[1]
+print(f"Input dimention: {input_dim}")
 
 model = VAE(input_dim, hidden_dim, latent_dim).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
@@ -58,7 +85,7 @@ print("TRAINING STARTED...")
 train_loss_vals2, val_loss_vals = train_with_KL_annelaing(model=model, optimizer=optimizer, scheduler=scheduler, n_epochs=n_epochs, train_loader=train_loader, val_loader=val_loader, beta_start=beta_start, beta_end=beta_end, max_norm=max_norm)
 
 # Save trained model
-torch.save(model.state_dict(), "saved_KL_annealing_VAE_BD.pt")
+torch.save(model.state_dict(), folder+"saved_KL_annealing_VAE_BD_100_new_2.pt")
 print("Model saved.")
 
 ## Generating a comparison graph 
@@ -66,21 +93,79 @@ print("GENERATING A COMPARISON GRAPH...")
 # Generating points for graphs
 epochs = np.linspace(1, n_epochs, num=n_epochs)
 # Plot train vs val loss graph
-name = "second_model_train_val_loss_BD.pdf"
+name = folder+"second_model_train_val_loss_BD_100_new_2.pdf"
 plot_loss_vs_epochs_graph(epochs=epochs, train_loss_vals=train_loss_vals2, val_loss_vals=val_loss_vals, fig_name=name)
 
+## Calculating F1 scores 
+# Clacualting F1 score over all samples 
+print("Calculating F1 scores...")
+model.eval()
+all_recon_x = []
+all_test_data = []
+
+# Ploting the data into batches
+with torch.no_grad():
+    for batch in test_loader:
+        batch_data = batch[0].to(device)
+        recon_x, mu, logvar = model(batch_data)
+        all_recon_x.append(recon_x.to(device))
+        all_test_data.append(batch_data.to(device))
+
+all_recon_x = torch.cat(all_recon_x)
+all_test_data = torch.cat(all_test_data)
+
+# Setting thershold 
+recon_x_binarized = (all_recon_x > 0.5).int()
+
+all_test_data_np = all_test_data.cpu().numpy().flatten()
+recon_x_binarized_np = recon_x_binarized.cpu().numpy().flatten()
+
+f1 = sklearn.metrics.f1_score(all_test_data_np, recon_x_binarized_np)
+print(f'F1 Score: {f1:.2f}')
+
+# Calcualting F1 score for each sample (comapring the target to reconstruction)
+f1_scores = []
+for genome_x, genome in zip(recon_x_binarized.cpu(), all_test_data.cpu().int()):
+    f1_scores.append(sklearn.metrics.f1_score(genome_x.numpy(), genome.numpy()))
+
+# Ploting a histogram of all calculated F1 scores 
+plt.figure(figsize=(10,8))
+plt.hist(f1_scores, color='dodgerblue')
+plt.xlabel("F1 score")
+plt.ylabel("Frequency")
+plt.savefig(folder+"f1_score_frequency_test_set_100_new_2.pdf", format="pdf", bbox_inches="tight")
+plt.show()
 
 ## Exploring latent space
 print("EXPLORING THE LATENT SPACE...")
 # Get latent variables
-latents = get_latent_variables(model, train_loader, device)
+latents = get_latent_variables(model, test_loader, device)
 
 # Apply t-SNE for dimensionality reduction
-name = "tsne_latent_space_visualisation_BD.pdf"
-do_tsne(n_components=2, latents=latents, fig_name=name)
+name = folder+"tsne_latent_space_visualisation_BD_100_new_2.pdf"
+# do_tsne(n_components=2, latents=latents, fig_name=name)
+tsne = TSNE(n_components=2)
+tsne_latents = tsne.fit_transform(latents)
+df_tsne = pd.DataFrame(tsne_latents, columns=['PC1', 'PC2'])
+print(f"len tsne latents: {len(tsne_latents)}")
+print(f"len test phylogroups: {len(test_phylogroups)}")
+df_tsne['phylogroup'] = test_phylogroups
+plt.figure(figsize=(10, 10))
+sns.scatterplot(x='PC1', y='PC2', hue = df_tsne['phylogroup'], data=df_tsne)
+plt.savefig(name, format="pdf", bbox_inches="tight")
+plt.show()
 
 # Apply PCA
-name = "pca_latent_space_visualisation_BD.pdf"
-do_pca(n_components=2, latents=latents, fig_name=name)
+name = folder+"pca_latent_space_visualisation_BD_100_new_2.pdf"
+# do_pca(n_components=2, latents=latents, fig_name=name)
+# Apply PCA
+pca = PCA(n_components=2)
+pca_latents = pca.fit_transform(latents)
+df_pca = pd.DataFrame(pca_latents, columns=['PC1', 'PC2'])
+df_pca['phylogroup'] = test_phylogroups
+plt.figure(figsize=(10, 10))
+sns.scatterplot(x='PC1', y='PC2', hue = df_pca['phylogroup'], data=df_pca)
+plt.savefig(name, format="pdf", bbox_inches="tight")
+plt.show()
 
 print("\n** SCRIPT IS DONE **")
