911a9eab7760e9f7a51b0901549365bd9b162c69
stacyshcherbakova <stacyshcherbakova@gmail.com>
2024-06-17 17:35:11 +0100

    version 3.0

--- phylogroups_processing.sh
+++ phylogroups_processing.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
+## Extracting metadata for a small dataset 
+# cd /Users/anastasiiashcherbakova/git_projects/masters_project/
+
+# cut -d$'\t' -f1,9 /Users/anastasiiashcherbakova/Desktop/diss_track/Additional_File_3/Supplementary_File_1.txt > data/accessionID_phylogroup.txt
+
+
+# head -n 1 accessionID_phylogroup.txt > data/accessionID_phylogroup2.txt
+
+# tail -n +2 accessionID_phylogroup.txt | while IFS=$'\t' read -r AccessionID Phylogroup; do
+#     AccessionID="${AccessionID:0:13}"
+#     echo -e "$AccessionID\t$Phylogroup"
+# done >> data/accessionID_phylogroup2.txt
+
+## Extracting metadata for a big dataset 
 cd /Users/anastasiiashcherbakova/git_projects/masters_project/
 
-cut -d$'\t' -f1,9 /Users/anastasiiashcherbakova/Desktop/diss_track/Additional_File_3/Supplementary_File_1.txt > accessionID_phylogroup.txt
-
-
-head -n 1 accessionID_phylogroup.txt > accessionID_phylogroup2.txt
-
-tail -n +2 accessionID_phylogroup.txt | while IFS=$'\t' read -r AccessionID Phylogroup; do
-    AccessionID="${AccessionID:0:13}"
-    echo -e "$AccessionID\t$Phylogroup"
-done >> accessionID_phylogroup2.txt
-
+cut -d$',' -f1,12 /Users/anastasiiashcherbakova/Desktop/diss_track/F1_genome_metadata.csv > data/accessionID_phylogroup_BD.txt
 
