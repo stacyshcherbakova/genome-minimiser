a3678e2d918b8c9336549ddf310bca1f51a7ce74
stacyshcherbakova <stacyshcherbakova@gmail.com>
2024-07-30 17:14:33 +0100

    updated paths and removed the data to generated genome to the data folder

--- genome_assessment/samples_into_sequences.py
+++ genome_assessment/samples_into_sequences.py
@@ -1,26 +1,16 @@
-### take the samples with the lowest number of genes and the highest number of essential genes 
-
-### going theough the sample - if the genes of index i is present (equal to 1 at the eindex) we find the gene name from the columns (it will be th ith column) and then take the name and search the fasts file for the sequence)
-
-### we find and add all the sequences that are present and create a new sequence
-
-### OR
-
-### we take the wild type of the e coli strain K12 MG1655, and go through the sample - if the genes of the index i is not present (equal to 0) we find the gene from the columns (it will be th ith column) and then take the name and search the fasts file for the sequnece
-
-### we then take the sequence and delete it from the wild type 
-
-### second approach is better 
+# We take the wild type of the e coli strain K12 MG1655, and go through the sample - if the genes of the index i is not present (equal to 0) we find the gene from the columns (it will be th ith column) and then take the name and search the fasts file for the sequnece
+# We then take the sequence and delete it from the wild type 
+# Second approach is better 
 
 from Bio import SeqIO
 import numpy as np
+from dir import *
 
-sequence_file = "/Users/anastasiiashcherbakova/Downloads/sequence.gb" 
-record = SeqIO.read(sequence_file, "genbank")
+record = SeqIO.read(WILD_TYPE_SEQUENCE, "genbank")
 original_genome_length = len(record.seq)
 # print(record)
 
-needed_genes = np.load('/Users/anastasiiashcherbakova/git_projects/masters_project/data/unique_gene_names.npy', allow_pickle=True).tolist()
+needed_genes = np.load(UNIQUE_GENE_NAMES, allow_pickle=True).tolist()
 # print(features_to_remove)
 
 non_essential_features = []
@@ -42,7 +32,7 @@ for feature in non_essential_features:
 
 new_sequence = ''.join(base for i, base in enumerate(record.seq) if i not in positions_to_remove)
 
-with open("minimized_genome.fasta", "w") as output_file:
+with open(MINIMIZED_GENOME, "w") as output_file:
     output_file.write(">Minimized_E_coli_K12_MG1655\n")
     output_file.write(str(new_sequence))
 
